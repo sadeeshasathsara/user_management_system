8c72ef1ddf3be5778a90702bf2901ed5d8efda78
Sadeesha Sathsara <sadeeshasathsara99@gmail.com>
2025-07-31 11:46:46 +0530

    route fix

--- express_backend/src/routes/api.route.js
+++ express_backend/src/routes/api.route.js
@@ -3,12 +3,16 @@ import { loginController } from '../controllers/login.controller.js';
 import { registerController } from '../controllers/register.controller.js';
 import { logoutController } from '../controllers/logout.controller.js';
 import { verifyAuth } from '../middleware/checkauth.middleware.js';
+import { createDepartment, getAllDepartments } from '../controllers/department.controller.js';
 
 const router = express.Router();
 
 router.post('/login', loginController);
 router.post('/register', registerController);
 router.get('/logout', logoutController);
+
+router.post('/department', createDepartment);
+router.get('/department', getAllDepartments)
 
 router.get('/check-auth', verifyAuth, (req, res) => {
     res.status(200).json({
